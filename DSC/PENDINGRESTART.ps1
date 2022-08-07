86ef88f22aae7b185621bd40bd02fa69f8c26343
github@mcitpnetworks.com <github@mcitpnetworks.com>
2022-08-07 19:23:45 -0400

    New Structure

---
+++ DSC/PENDINGRESTART.ps1
@@ -0,0 +1,16 @@
+﻿configuration PENDINGRESTART
+{
+   param
+   (
+    )
+
+    Import-DscResource -Module ComputerManagementDsc # Used for Reboots
+
+    Node localhost
+    {
+        PendingReboot Reboot
+        {
+           Name = "Reboot"
+        }
+    }
+}
