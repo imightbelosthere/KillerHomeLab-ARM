c3af7d613433068aaf4231288b47dbd85e45d4a5
github@mcitpnetworks.com <github@mcitpnetworks.com>
2022-08-12 11:09:27 -0400

    New Structure

---
+++ DSC/DEVOPSSERVER22.ps1
@@ -0,0 +1,125 @@
+﻿configuration DEVOPSSERVER22
+{
+   param
+   (
+        [String]$DevOpsServerSASUrl, 
+        [String]$TimeZone,                       
+        [String]$NetBiosDomain,
+        [System.Management.Automation.PSCredential]$Admincreds
+    )
+
+    Import-DscResource -Module xPSDesiredStateConfiguration # Used for xRemoteFile
+    Import-DscResource -Module ComputerManagementDsc # Used for TimeZone
+    Import-DscResource -Module xStorage # Used by Disk
+
+    [System.Management.Automation.PSCredential ]$DomainCreds = New-Object System.Management.Automation.PSCredential ("${NetBiosDomain}\$($Admincreds.UserName)", $Admincreds.Password)
+
+    Node localhost
+    {
+        Registry SchUseStrongCrypto
+        {
+            Key                         = 'HKLM:\SOFTWARE\Microsoft\.NETFramework\v4.0.30319'
+            ValueName                   = 'SchUseStrongCrypto'
+            ValueType                   = 'Dword'
+            ValueData                   =  '1'
+            Ensure                      = 'Present'
+        }
+
+        Registry SchUseStrongCrypto64
+        {
+            Key                         = 'HKLM:\SOFTWARE\Wow6432Node\Microsoft\.NETFramework\v4.0.30319'
+            ValueName                   = 'SchUseStrongCrypto'
+            ValueType                   = 'Dword'
+            ValueData                   =  '1'
+            Ensure                      = 'Present'
+        }
+
+        WindowsFeature 'NetFramework45'
+        {
+            Name   = 'NET-Framework-45-Core'
+            Ensure = 'Present'
+        }
+
+        Script DismountISO
+        {
+      	    SetScript = {
+                Dismount-DiskImage "C:\DevOpsServerInstall\DevOpsServer2022.iso" -ErrorAction 0
+            }
+            GetScript =  { @{} }
+            TestScript = { $false }
+        }
+
+        TimeZone SetTimeZone
+        {
+            IsSingleInstance = 'Yes'
+            TimeZone         = $TimeZone
+        }
+
+        File CreateSoftwareFolder
+        {
+            Type = 'Directory'
+            DestinationPath = 'C:\DevOpsServerInstall'
+            Ensure = "Present"
+        }
+
+        xRemoteFile SSMS
+        {
+            DestinationPath = 'C:\DevOpsServerInstall\SSMS-Setup-ENU.exe'
+            Uri             = "https://aka.ms/SSMSFullSetup"
+            UserAgent       = "[Microsoft.PowerShell.Commands.PSUserAgent]::InternetExplorer"
+            DependsOn =    "[File]CreateSoftwareFolder"
+        }
+
+        Script InstallSQLManagementStudio
+        {
+            SetScript =
+            {
+                # Install SQL Management Studio
+                C:\SQLSoftware\SSMS-Setup-ENU.exe /install /quiet /norestart
+
+                $EnableSQL = Get-NetFirewallRule "SQL-In-TCP" -ErrorAction 0
+                IF ($EnableSQL -eq $null) {New-NetFirewallRule -DisplayName "SQL-In-TCP" -Direction Inbound -LocalPort 1433 -Protocol TCP -Action Allow}
+            }
+            GetScript =  { @{} }
+            TestScript = { $false}
+            DependsOn = '[xRemoteFile]SSMS'
+        }
+
+        xRemoteFile DownloadDevOpsServer2022
+        {
+            DestinationPath = "C:\DevOpsServerInstall\DevOpsServer2022.iso"
+            Uri             = "$DevOpsServerSASUrl"
+            UserAgent       = "[Microsoft.PowerShell.Commands.PSUserAgent]::InternetExplorer"
+            DependsOn =    '[File]CreateSoftwareFolder', '[Script]InstallSQLManagementStudio'
+        }
+
+        xMountImage DevOpsServerISO
+        {
+            ImagePath   = 'C:\DevOpsServerInstall\DevOpsServer2022.iso'
+            DriveLetter = 'I'
+            DependsOn = '[xRemoteFile]DownloadDevOpsServer2022'
+        }
+
+        xWaitForVolume WaitForISO
+        {
+            DriveLetter      = 'I'
+            RetryIntervalSec = 5
+            RetryCount       = 10
+        }
+
+        Script InstallDevOpsServer2022Bits
+        {
+            SetScript =
+            {
+                $Install = Get-ChildItem -Path C:\DevOpsServerInstall\DeployDevOpsServer.cmd -ErrorAction 0
+                IF ($Install -eq $null) {                 
+                Set-Content -Path C:\DevOpsServerInstall\DeployDevOpsServer.cmd -Value "I:\AzureDevops2022_RC1.exe /Silent"
+                C:\DevOpsServerInstall\DeployDevOpsServer.cmd
+                }
+            }
+            GetScript =  { @{} }
+            TestScript = { $false}
+            DependsOn = "[xWaitForVolume]WaitForISO"
+        }
+    }
+}
---
+++ DSC/SSMS.ps1
@@ -0,0 +1,74 @@
+﻿Configuration SSMS
+{
+   param
+   (
+        [String]$TimeZone,
+        [System.Management.Automation.PSCredential]$Admincreds
+    )
+ 
+    Import-DscResource -ModuleName ComputerManagementDsc # Used for TimeZone
+    Import-DscResource -ModuleName xPSDesiredStateConfiguration # Used for xRemote
+
+    Node localhost
+    {
+        Registry SchUseStrongCrypto
+        {
+            Key                         = 'HKLM:\SOFTWARE\Microsoft\.NETFramework\v4.0.30319'
+            ValueName                   = 'SchUseStrongCrypto'
+            ValueType                   = 'Dword'
+            ValueData                   =  '1'
+            Ensure                      = 'Present'
+        }
+
+        Registry SchUseStrongCrypto64
+        {
+            Key                         = 'HKLM:\SOFTWARE\Wow6432Node\Microsoft\.NETFramework\v4.0.30319'
+            ValueName                   = 'SchUseStrongCrypto'
+            ValueType                   = 'Dword'
+            ValueData                   =  '1'
+            Ensure                      = 'Present'
+        }
+
+        WindowsFeature 'NetFramework45'
+        {
+            Name   = 'NET-Framework-45-Core'
+            Ensure = 'Present'
+        }
+
+        File SQLSoftware
+        {
+            Type = 'Directory'
+            DestinationPath = 'C:\SQLSoftware'
+            Ensure = "Present"
+        }
+
+        xRemoteFile SSMS
+        {
+            DestinationPath = 'C:\SQLSoftware\SSMS-Setup-ENU.exe'
+            Uri             = "https://aka.ms/SSMSFullSetup"
+            UserAgent       = "[Microsoft.PowerShell.Commands.PSUserAgent]::InternetExplorer"
+            DependsOn =    "[File]SQLSoftware"
+        }
+
+        Script InstallSQLManagementStudio
+        {
+            SetScript =
+            {
+                # Install SQL Management Studio
+                C:\SQLSoftware\SSMS-Setup-ENU.exe /install /quiet /norestart
+
+                $EnableSQL = Get-NetFirewallRule "SQL-In-TCP" -ErrorAction 0
+                IF ($EnableSQL -eq $null) {New-NetFirewallRule -DisplayName "SQL-In-TCP" -Direction Inbound -LocalPort 1433 -Protocol TCP -Action Allow}
+            }
+            GetScript =  { @{} }
+            TestScript = { $false}
+            DependsOn = '[xRemoteFile]SSMS'
+        }
+
+        TimeZone SetTimeZone
+        {
+            IsSingleInstance = 'Yes'
+            TimeZone         = $TimeZone
+        }
+     }
+  }
