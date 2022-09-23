4058d300f4c38366ec6074b9f8e1fa27a88e6a8d
dom-admin@sub1.elliottfieldsjr.us <dom-admin@sub1.elliottfieldsjr.us>
2022-09-22 21:44:40 -0400

    Latest Build

--- DSC/PREPARENDES.ps1
+++ DSC/PREPARENDES.ps1
@@ -84,7 +84,9 @@
             SetScript =
             {
                 $AccountCheck = Get-LocalGroupMember -Group IIS_IUSRS -Member "$using:NetBiosDomain\$using:Account" -ErrorAction 0
-                IF ($AccountCheck -eq $null){Add-LocalGroupMember -Member "$using:NetBiosDomain\$using:Account" -Group IIS_IUSRS}
+                IF ($AccountCheck -eq $null){
+                    Add-LocalGroupMember -Member "$using:NetBiosDomain\$using:Account" -Group IIS_IUSRS
+                }
             }
             GetScript =  { @{} }
             TestScript = { $false}
@@ -96,8 +98,8 @@
             {
                 $file = Get-Item -Path "C:\NDES-Software\SETSPN.cmd" -ErrorAction 0
                 IF ($file -eq $Null){
-                Set-Content -Path C:\NDES-Software\SETSPN.cmd -Value "c:\windows\system32\setspn.exe -f -s http/$using:computerName.$using:InternalDomainName $using:NetBiosDomain\$using:Account"
-                C:\NDES-Software\SETSPN.cmd
+                    Set-Content -Path C:\NDES-Software\SETSPN.cmd -Value "c:\windows\system32\setspn.exe -f -s http/$using:computerName.$using:InternalDomainName $using:NetBiosDomain\$using:Account"
+                    C:\NDES-Software\SETSPN.cmd
                 }
             }
             GetScript =  { @{} }
@@ -110,10 +112,23 @@
         {
             SetScript =
             {
-                $LoadCreds = "$using:AdminCreds"
-                $Password = $AdminCreds.Password
+                $Load = "$using:DomainCreds"
+                $Domain = $DomainCreds.GetNetworkCredential().Domain
+                $Username = $DomainCreds.GetNetworkCredential().UserName
+                $Password = $DomainCreds.GetNetworkCredential().Password
 
-                Install-AdcsNetworkDeviceEnrollmentService -ServiceAccountName "$using:NetBiosDomain\$using:Account" -ServiceAccountPassword $Password -CAConfig "$using:EnterpriseCAServer\$using:EnterpriseCAName" -RAName "$using:NamingConvention-NDES-RA" -RACountry "US" -RACompany "$using:NamingConvention" -SigningProviderName "Microsoft Strong Cryptographic Provider" -SigningKeyLength 4096 -EncryptionProviderName "Microsoft Strong Cryptographic Provider" -EncryptionKeyLength 4096
+                $file = Get-Item -Path "C:\NDES-Software\Install_NDES.ps1" -ErrorAction 0
+                IF ($file -eq $Null){
+                    Set-Content -Path C:\NDES-Software\Install_NDES.ps1 -Value "Install-AdcsNetworkDeviceEnrollmentService -ServiceAccountName $using:NetBiosDomain\$using:Account -ServiceAccountPassword $Password -CAConfig $using:EnterpriseCAServer\$using:EnterpriseCAName -RAName $using:NamingConvention-NDES-RA -RACountry 'US' -RACompany $using:NamingConvention -SigningProviderName 'Microsoft Strong Cryptographic Provider' -SigningKeyLength 4096 -EncryptionProviderName 'Microsoft Strong Cryptographic Provider' -EncryptionKeyLength 4096"
+                }
+
+                # Install NDES
+                $scheduledtask = Get-ScheduledTask "Install NDES" -ErrorAction 0
+                $action = New-ScheduledTaskAction -Execute Powershell -Argument '.\Install_NDES.ps1' -WorkingDirectory 'C:\NDES-Software'
+                IF ($scheduledtask -eq $null) {
+                    Register-ScheduledTask -Action $action -TaskName "Install NDES" -Description "Install NDES Role" -User $Domain\$Username -Password $Password
+                    Start-ScheduledTask "Install NDES"
+                }
             }
             GetScript =  { @{} }
             TestScript = { $false}
