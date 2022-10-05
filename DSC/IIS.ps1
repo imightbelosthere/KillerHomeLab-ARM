e9ed08ffd8531703f51e1817d206bf7b3fa94278
dom-admin@sub1.elliottfieldsjr.us <dom-admin@sub1.elliottfieldsjr.us>
2022-10-04 21:52:26 -0400

    New Structure

---
+++ DSC/IIS.ps1
@@ -0,0 +1,23 @@
+﻿configuration IIS
+{
+    Node localhost
+    {
+        LocalConfigurationManager
+        {
+            RebootNodeIfNeeded = $true
+        }
+        
+        WindowsFeature Web-Mgmt-Console
+        {
+            Ensure = 'Present'
+            Name = 'Web-Mgmt-Console'
+        }
+        
+        WindowsFeature Web-Server
+        {
+            Ensure = 'Present'
+            Name = 'Web-Server'
+            IncludeAllSubFeature = $true
+        }
+    }
+}
