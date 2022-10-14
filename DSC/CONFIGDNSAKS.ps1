83912ae35d98efd0ee5758f63cca946d5f10e5c0
dom-admin@sub1.elliottfieldsjr.us <dom-admin@sub1.elliottfieldsjr.us>
2022-10-14 15:20:25 -0400

    New Structure

---
+++ DSC/CONFIGDNSAKS.ps1
@@ -0,0 +1,20 @@
+﻿configuration CONFIGDNSAKS
+{
+   param
+   (
+        [String]$DNSIP,                                       
+        [String]$AKSDNSZone
+    )
+
+    Import-DscResource -ModuleName DnsServerDsc
+
+    Node localhost
+    {
+        DnsServerConditionalForwarder AKSDnsZone
+        {
+            Name      = $AKSDNSZone
+            MasterServers = $DNSIP
+            Ensure    = 'Present'
+        }
+    }
+}
