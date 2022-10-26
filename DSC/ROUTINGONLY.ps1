3078b3aac306e888ee389350cc40042f7238e48c
dom-admin@sub1.elliottfieldsjr.us <dom-admin@sub1.elliottfieldsjr.us>
2022-10-26 09:58:08 -0400

    New Structure

---
+++ DSC/ROUTINGONLY.ps1
@@ -0,0 +1,61 @@
+﻿configuration ROUTINGONLY
+{
+   param
+   (
+        [String]$ComputerName,
+        [System.Management.Automation.PSCredential]$Admincreds                                  
+    )
+
+    [System.Management.Automation.PSCredential ]$LocalCreds = New-Object System.Management.Automation.PSCredential ("${ComputerName}\$($AdminCreds.UserName)", $AdminCreds.Password)
+
+    Node localhost
+    {
+        File ConfigureRRAS
+        {
+            Type = 'Directory'
+            DestinationPath = 'C:\ConfigureRRAS'
+            Ensure = "Present"
+        }
+
+        Script ConfigureRRAS
+        {
+            SetScript =
+            {
+                # Create ConfigureRRAS Script
+                $FalseValue = '$False'
+                Set-Content -Path C:\ConfigureRRAS\SetupRRAS.ps1 -Value "Install-RemoteAccess -VpnType RoutingOnly"
+                Add-Content -Path C:\ConfigureRRAS\SetupRRAS.ps1 -Value "Import-Module RemoteAccess"
+            }
+            GetScript =  { @{} }
+            TestScript = { $false}
+            DependsOn = '[File]ConfigureRRAS'
+        }
+
+        ScheduledTask CreateConfigureRRAS
+        {
+            TaskName            = 'Configure RRAS'
+            ActionExecutable    = 'C:\windows\system32\WindowsPowerShell\v1.0\powershell.exe'
+            ScheduleType        = 'Once'
+            StartTime           = (Get-Date).AddMinutes(1)
+            ActionArguments     = 'C:\ConfigureRRAS\SetupRRAS.ps1'
+            Enable              = $true
+            ExecuteAsCredential = $LocalCreds
+            LogonType           = 'Password'
+            DependsOn = '[Script]ConfigureRRAS'
+        }
+
+        Script AllowLegacy
+        {
+            SetScript =
+            {
+                # Allow Remote Copy
+                $allowlegacy = get-itemproperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\RemoteAccess\Parameters" -Name "ModernStackEnabled" -ErrorAction 0
+                IF ($allowlegacy.ModernStackEnabled -ne 0) {New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\RemoteAccess\Parameters\" -Name "ModernStackEnabled" -Value 0 -Force}
+            }
+            GetScript =  { @{} }
+            TestScript = { $false}
+            DependsOn = '[ScheduledTask]CreateConfigureRRAS'
+        }
+
+    }
+}
