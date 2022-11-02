4da81e5ea9aee6fd8d1dd1bc8b3ae06eb99afd8f
dom-admin@sub1.elliottfieldsjr.us <dom-admin@sub1.elliottfieldsjr.us>
2022-11-02 13:36:49 -0400

    New Structure

--- DSC/SQLACCOUNTS.ps1
+++ DSC/SQLACCOUNTS.ps1
@@ -43,30 +43,6 @@
             }
         }
 
-        Script GrantCreateComputerAccounts
-        {
-            SetScript =
-            {
-
-                $acl = get-acl "ad:$using:BaseDN"
-                $User = Get-ADUser "$using:InstallAccount"
-
-                # The following object specific ACE is to grant Group permission to change user password on all user objects under OU
-                $objectguid = new-object Guid bf967a86-0de6-11d0-a285-00aa003049e2 # is the rightsGuid for the extended right Create Computer Account
-                $inheritedobjectguid = new-object Guid $User.ObjectGUID # is the schemaIDGuid for the user
-                $identity = [System.Security.Principal.IdentityReference] $User.SID
-                $adRights = [System.DirectoryServices.ActiveDirectoryRights] "GenericAll"
-                $type = [System.Security.AccessControl.AccessControlType] "Allow"
-                $inheritanceType = [System.DirectoryServices.ActiveDirectorySecurityInheritance] "All"
-                $ace = new-object System.DirectoryServices.ActiveDirectoryAccessRule $identity,$adRights,$type,$objectGuid,$inheritanceType,$inheritedobjectguid
-                $acl.AddAccessRule($ace)
-                Set-acl -aclobject $acl "ad:$using:BaseDN"
-            }
-            GetScript =  { @{} }
-            TestScript = { $false}
-            DependsOn = '[ADUser]Install'
-        }
-
     }
 }
 
