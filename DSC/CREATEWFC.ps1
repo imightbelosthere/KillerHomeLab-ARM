ba9109c49c05192ea98c118f67e0bb06b58305d7
dom-admin@sub1.elliottfieldsjr.us <dom-admin@sub1.elliottfieldsjr.us>
2022-11-04 20:36:49 -0400

    New Structure

--- DSC/CREATEWFC.ps1
+++ DSC/CREATEWFC.ps1
@@ -7,13 +7,13 @@
         [System.Management.Automation.PSCredential]$Admincreds
     )
 
-    Import-DscResource -Module xFailoverCluster # Used for Reboots
+    Import-DscResource -Module FailoverClusterdSC # Used for Reboots
 
     [System.Management.Automation.PSCredential ]$DomainCreds = New-Object System.Management.Automation.PSCredential ("${NetBiosDomain}\$($Admincreds.UserName)", $Admincreds.Password)
 
     Node localhost
     {
-        xCluster CreateCluster
+        Cluster CreateCluster
         {
             Name = $SQLClusterName
             DomainAdministratorCredential = $DomainCreds
