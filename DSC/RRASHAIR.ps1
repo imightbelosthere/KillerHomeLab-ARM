0c2a46c2af645c24fc93178de70c77bfb7cb490d
dom-admin@sub1.elliottfieldsjr.us <dom-admin@sub1.elliottfieldsjr.us>
2022-11-19 16:32:41 -0500

    New Structure

---
+++ DSC/RRASHAIR.ps1
@@ -0,0 +1,48 @@
+﻿Configuration RRASHAIR
+{
+   param
+   (
+        [String]$Site1NextHop,
+        [String]$Site2NextHop,
+        [String]$OutsideSubnetPrefix,
+        [String]$InsideSubnetPrefix,
+        [String]$Site1VNet,
+        [String]$Site2VNet
+    )
+
+    Node localhost
+    {   
+        WindowsFeature Routing
+        { 
+            Ensure = 'Present' 
+            Name = 'Routing' 
+        }         
+
+        WindowsFeature RSAT-RemoteAccess
+        { 
+            Ensure = 'Present' 
+            Name = 'RSAT-RemoteAccess' 
+            DependsOn = '[WindowsFeature]Routing'
+        }
+
+       Script AddStaticRoutes
+        {
+            SetScript =
+            {
+                $Site1RouteCheck = Get-NetRoute | Where-Object {$_.NextHop -like "$using:Site1NextHop"}
+                IF ($Site1RouteCheck -eq $Null) {
+                    $OutsideAdapter = Get-NetIPAddress | Where-Object {$_.IPAddress -like "$using:OutsideSubnetPrefix"+"*"}
+                    New-NetRoute -DestinationPrefix "$using:Site1VNet" -InterfaceIndex $OutsideAdapter.InterfaceIndex -NextHop "$using:Site1NextHop"
+                }
+
+                $Site2RouteCheck = Get-NetRoute | Where-Object {$_.NextHop -like "$using:Site2NextHop"}
+                IF ($Site2RouteCheck -eq $Null) {
+                    $OutsideAdapter = Get-NetIPAddress | Where-Object {$_.IPAddress -like "$using:OutsideSubnetPrefix"+"*"}
+                    New-NetRoute -DestinationPrefix "$using:Site2VNet" -InterfaceIndex $OutsideAdapter.InterfaceIndex -NextHop "$using:Site2NextHop"
+                }
+            }
+            GetScript =  { @{} }
+            TestScript = { $false}
+        }
+     }
+  }
