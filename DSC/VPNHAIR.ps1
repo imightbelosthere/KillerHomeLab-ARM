1d122df19b9c361dce3b5c093924ec08c489ff05
dom-admin@sub1.elliottfieldsjr.us <dom-admin@sub1.elliottfieldsjr.us>
2022-11-20 21:31:29 -0500

    New Structure

---
+++ DSC/VPNHAIR.ps1
@@ -0,0 +1,93 @@
+﻿configuration VPNHAIR
+{
+   param
+   (
+        [String]$ComputerName,
+        [String]$Site1Name,
+        [String]$Site2Name,
+        [String]$Site1RemoteGatewayIP,
+        [String]$Site2RemoteGatewayIP,
+        [String]$Site1IPv4Subnet,
+        [String]$Site2IPv4Subnet,
+        [String]$SharedKey,
+        [System.Management.Automation.PSCredential]$Admincreds                                  
+    )
+
+    Import-DscResource -Module ComputerManagementDsc # Task Scheduler
+
+    [System.Management.Automation.PSCredential ]$LocalCreds = New-Object System.Management.Automation.PSCredential ("${ComputerName}\$($AdminCreds.UserName)", $AdminCreds.Password)
+
+    Node localhost
+    {
+        File ConfigureRRASDirectory
+        {
+            Type = 'Directory'
+            DestinationPath = 'C:\ConfigureRRAS'
+            Ensure = "Present"
+        }
+
+        Script ConfigureRRAS
+        {
+            SetScript =
+            {
+                # Create ConfigureRRAS Script
+                $FalseValue = '$False'
+                $ServiceValue = '$Service'
+                $InstallStatusValue = '$InstallStatus'
+                $VPNINTSITE1VALUE = '$VPNINTSITE1'
+                $VPNINTSITE2VALUE = '$VPNINTSITE2'
+                Set-Content -Path C:\ConfigureRRAS\SetupRRAS.ps1 -Value '$RemoteAccess = Get-RemoteAccess'
+                Add-Content -Path C:\ConfigureRRAS\SetupRRAS.ps1 -Value "IF ($RemoteAccessValue.VpnS2SStatus -ne 'Installed'){"
+                Add-Content -Path C:\ConfigureRRAS\SetupRRAS.ps1 -Value 'Install-RemoteAccess -VpnType VpnS2S'
+                Add-Content -Path C:\ConfigureRRAS\SetupRRAS.ps1 -Value '$RemoteAccess = Get-RemoteAccess'
+                Add-Content -Path C:\ConfigureRRAS\SetupRRAS.ps1 -Value '$InstallStatus = $RemoteAccess.VpnS2SStatus'
+                Add-Content -Path C:\ConfigureRRAS\SetupRRAS.ps1 -Value "while (($ServiceValue.Status -ne 'Running')){sleep 10}"
+                Add-Content -Path C:\ConfigureRRAS\SetupRRAS.ps1 -Value "while (($InstallStatusValue -ne 'Installed')){sleep 10}"
+                Add-Content -Path C:\ConfigureRRAS\SetupRRAS.ps1 -Value 'sleep 30'
+                Add-Content -Path C:\ConfigureRRAS\SetupRRAS.ps1 -Value '}'
+                Add-Content -Path C:\ConfigureRRAS\SetupRRAS.ps1 -Value '$VPNINT = Get-VpnS2SInterface'
+                Add-Content -Path C:\ConfigureRRAS\SetupRRAS.ps1 -Value 'IF ($VPNINT -ne $null){'
+                Add-Content -Path C:\ConfigureRRAS\SetupRRAS.ps1 -Value "$VPNINTSITE1VALUE = Get-VpnS2SInterface -Name $using:Site1Name -ErrorAction 0"
+                Add-Content -Path C:\ConfigureRRAS\SetupRRAS.ps1 -Value "$VPNINTSITE2VALUE = Get-VpnS2SInterface -Name $using:Site2Name -ErrorAction 0"
+                Add-Content -Path C:\ConfigureRRAS\SetupRRAS.ps1 -Value '}'
+                Add-Content -Path C:\ConfigureRRAS\SetupRRAS.ps1 -Value 'IF ($VPNINTSITE1VALUE -eq $null){'
+                Add-Content -Path C:\ConfigureRRAS\SetupRRAS.ps1 -Value "Add-VpnS2SInterface -Protocol IKEv2 -AuthenticationMethod PSKOnly -NumberOfTries 3 -ResponderAuthenticationMethod PSKOnly -Name $using:Site1Name -Destination $using:Site1RemoteGatewayIP -IPv4Subnet $using:Site1IPv4Subnet:100 -SharedSecret $using:SharedKey"
+                Add-Content -Path C:\ConfigureRRAS\SetupRRAS.ps1 -Value '}'
+                Add-Content -Path C:\ConfigureRRAS\SetupRRAS.ps1 -Value 'IF ($VPNINTSITE2VALUE -eq $null){'
+                Add-Content -Path C:\ConfigureRRAS\SetupRRAS.ps1 -Value "Add-VpnS2SInterface -Protocol IKEv2 -AuthenticationMethod PSKOnly -NumberOfTries 3 -ResponderAuthenticationMethod PSKOnly -Name $using:Site2Name -Destination $using:Site2RemoteGatewayIP -IPv4Subnet $using:Site2IPv4Subnet:100 -SharedSecret $using:SharedKey"
+                Add-Content -Path C:\ConfigureRRAS\SetupRRAS.ps1 -Value '}'
+
+            }
+            GetScript =  { @{} }
+            TestScript = { $false}
+            DependsOn = '[File]ConfigureRRASDirectory'
+        }
+
+        ScheduledTask CreateConfigureRRAS
+        {
+            TaskName            = 'Configure RRAS'
+            ActionExecutable    = 'C:\windows\system32\WindowsPowerShell\v1.0\powershell.exe'
+            ScheduleType        = 'Once'
+            StartTime           = (Get-Date).AddMinutes(1)
+            ActionArguments     = 'C:\ConfigureRRAS\SetupRRAS.ps1'
+            Enable              = $true
+            ExecuteAsCredential = $LocalCreds
+            LogonType           = 'Password'
+            DependsOn = '[Script]ConfigureRRAS'
+        }
+
+        Script AllowLegacy
+        {
+            SetScript =
+            {
+                # Allow Remote Copy
+                $allowlegacy = get-itemproperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\RemoteAccess\Parameters" -Name "ModernStackEnabled" -ErrorAction 0
+                IF ($allowlegacy.ModernStackEnabled -ne 0) {New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\RemoteAccess\Parameters\" -Name "ModernStackEnabled" -Value 0 -Force}
+            }
+            GetScript =  { @{} }
+            TestScript = { $false}
+            DependsOn = '[ScheduledTask]CreateConfigureRRAS'
+        }
+
+    }
+}
