4a2a5df63ca4da119e7091216d056aa2b3c2bf8a
dom-admin@sub1.elliottfieldsjr.us <elliottf@microsoft.com>
2023-02-25 00:30:13 -0500

    Updated Restart

--- DSC/RESTARTVM2.ps1
+++ DSC/RESTARTVM2.ps1
@@ -2,71 +2,24 @@
 {
    param
    (
-        [String]$Identifier,
-        [String]$DomainName,
-        [System.Management.Automation.PSCredential]$Admincreds
+        [String]$Identifier
     )
-    
-    [System.Management.Automation.PSCredential ]$DomainCredsFQDN = New-Object System.Management.Automation.PSCredential ("$($Admincreds.UserName)@$($DomainName)", $Admincreds.Password)
-
-    Import-DscResource -Module ComputerManagementDsc # Used for Reboots
-
-    $DomainMembership = (Get-WmiObject -Class Win32_ComputerSystem).PartOfDomain
 
     Node localhost
     {
-        LocalConfigurationManager
+        Script Reboot
         {
-            RebootNodeIfNeeded = $true
-            ActionAfterReboot = "StopConfiguration"
-            ConfigurationMode = "ApplyOnly"
-
-        }
-
-        if ($DomainMembership -eq 'False')
-        {
-            Script RebootNonDomain
+            SetScript = 
             {
-                TestScript = {
-                return (Test-Path "HKLM:\SOFTWARE\MyMainKey\$using:Identifier")
-                }
-                SetScript = {
+                    $RegEntry = Get-Item -Path "HKLM:\SOFTWARE\MyMainKey\$using:Identifier" -ErrorAction 0
+                    IF ($RegEntry -eq $null){
                         Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Services\WindowsAzureGuestAgent' -Name DependOnService -Type MultiString -Value Dnscache
 			            New-Item -Path "HKLM:\SOFTWARE\MyMainKey\$using:Identifier" -Force
-			            $global:DSCMachineStatus = 1 
-                    }
-                GetScript = { return @{result = 'result'}}
-                Credential = $Admincreds
+                        Restart-Computer -Force
+                    } 
             }
-
-            PendingReboot RebootNonDomain
-            {
-               Name = "Reboot"
-               DependsOn = '[Script]RebootNonDomain'
-            }
-        }
-
-        else
-        {
-            Script RebootDomain
-            {
-                TestScript = {
-                return (Test-Path "HKLM:\SOFTWARE\MyMainKey\$using:Identifier")
-                }
-                SetScript = {
-                        Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Services\WindowsAzureGuestAgent' -Name DependOnService -Type MultiString -Value Dnscache
-			            New-Item -Path "HKLM:\SOFTWARE\MyMainKey\$using:Identifier" -Force
-			            $global:DSCMachineStatus = 1 
-                    }
-                GetScript = { return @{result = 'result'}}
-                PsDscRunAsCredential = $DomainCredsFQDN
-            }
-
-            PendingReboot RebootDomain
-            {
-               Name = "Reboot"
-               DependsOn = '[Script]RebootDomain'
-            }
+            GetScript =  { @{} }
+            TestScript = { $false}
         }
     }
 }
