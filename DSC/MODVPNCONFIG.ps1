b67bbf03aabdb3a7372a61b8e0a2f4ec7a1f69b0
dom-admin@sub1.elliottfieldsjr.us <elliottf@microsoft.com>
2023-03-05 05:09:32 -0500

    Update Deployment

--- DSC/MODVPNCONFIG.ps1
+++ DSC/MODVPNCONFIG.ps1
@@ -22,9 +22,10 @@
                 $RemoteAccessValue = '$RemoteAccess'
                 $AZUREIPv4 = "$using:AZUREIPv4Subnet"+':100'
                 $AZURETRANSITIPv4 = "$using:AZURETRANSITIPv4Subnet"+':100'
+                $IPV4Subnet = '@("' + $AZUREIPv4 + '","' + $AZURETRANSITIPv4 + '")'
 
                 Set-Content -Path C:\ConfigureRRAS\MODVPNConfig.ps1 -Value '$RemoteAccess = Get-RemoteAccess'
-                Add-Content -Path C:\ConfigureRRAS\MODVPNConfig.ps1 -Value "Get-VpnS2SInterface | Set-VpnS2SInterface -IPv4Subnet @($AZUREIPv4,$AZURETRANSITIPv4)"
+                Add-Content -Path C:\ConfigureRRAS\MODVPNConfig.ps1 -Value "Get-VpnS2SInterface | Set-VpnS2SInterface -IPv4Subnet $IPV4Subnet"
             }
             GetScript =  { @{} }
             TestScript = { $false}
