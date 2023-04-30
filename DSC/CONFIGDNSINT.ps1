eeff9f06d3b5f9da4a1a89947d480126eca85cbb
dom-admin@sub1.elliottfieldsjr.us <elliottf@microsoft.com>
2023-04-29 23:53:35 -0400

    Updated CONFIGDNSINT

--- DSC/CONFIGDNSINT.ps1
+++ DSC/CONFIGDNSINT.ps1
@@ -11,11 +11,6 @@
 
     Import-DscResource -ModuleName DnsServerDsc
 
-    $Server = Get-ComputerInfo
-    $ServerName = $Server.CsDNSHostName
-    $DomainName = $Server.CsDomain
-    $FQDN = $ServerName + '.' + $DomainName
-
     Node localhost
     {
         LocalConfigurationManager
@@ -24,39 +19,54 @@
             ConfigurationMode = "ApplyOnly"
         }
 
-        $Service = Get-Service -Name NTDS
-        while (($Service.Status -ne 'Running')){
-            Start-Sleep 10
-            $Service = Get-Service -Name NTDS
-        }
+        Script CheckDNS
+        {
+            SetScript =
+            {
 
-        $Forest = Get-ADForest -ErrorAction 0
-        while (($Forest -eq $null)){
-            Start-Sleep 10
-            $Forest = Get-ADForest -ErrorAction 0
-        }
-        $SchemaMaster = $Forest.SchemaMaster
+                $Server = Get-ComputerInfo
+                $ServerName = $Server.CsDNSHostName
+                $DomainName = $Server.CsDomain
+                $FQDN = $ServerName + '.' + $DomainName
 
-        IF ($FQDN -eq $SchemaMaster){
-            $DomainEvent = Get-EventLog -LogName "DNS Server" -ErrorAction 0 | Where-Object {($_.InstanceId -like 4500) -and ($_.Message -like '*Domain*')}
-            while (($DomainEvent -eq $null)){
-                Start-Sleep 10
-                $DomainEvent = Get-EventLog -LogName "DNS Server" -ErrorAction 0 | Where-Object {($_.InstanceId -like 4500) -and ($_.Message -like '*Domain*')}
+                $Service = Get-Service -Name NTDS
+                while (($Service.Status -ne 'Running')){
+                    Start-Sleep 10
+                    $Service = Get-Service -Name NTDS
+                }
+
+                $Forest = Get-ADForest -ErrorAction 0
+                while (($Forest -eq $null)){
+                    Start-Sleep 10
+                    $Forest = Get-ADForest -ErrorAction 0
+                }
+                $SchemaMaster = $Forest.SchemaMaster
+
+                IF ($FQDN -eq $SchemaMaster){
+                    $DomainEvent = Get-EventLog -LogName "DNS Server" -ErrorAction 0 | Where-Object {($_.InstanceId -like 4500) -and ($_.Message -like '*Domain*')}
+                    while (($DomainEvent -eq $null)){
+                        Start-Sleep 10
+                        $DomainEvent = Get-EventLog -LogName "DNS Server" -ErrorAction 0 | Where-Object {($_.InstanceId -like 4500) -and ($_.Message -like '*Domain*')}
+                    }
+
+                    $ForestEvent = Get-EventLog -LogName "DNS Server" -ErrorAction 0 | Where-Object {($_.InstanceId -like 4500) -and ($_.Message -like '*Forest*')}
+                    while (($ForestEvent -eq $null)){
+                        Start-Sleep 10
+                        $ForestEvent = Get-EventLog -LogName "DNS Server" -ErrorAction 0 | Where-Object {($_.InstanceId -like 4500) -and ($_.Message -like '*Forest*')}
+                    }
+                }
+
+                ELSE {
+                    $DNSLoadedEvent = Get-EventLog -LogName "DNS Server" -ErrorAction 0 | Where-Object {($_.InstanceId -like 4)}
+                    while (($DNSLoadedEvent -eq $null)){
+                        Start-Sleep 10
+                        $DNSLoadedEvent = Get-EventLog -LogName "DNS Server" -ErrorAction 0 | Where-Object {($_.InstanceId -like 4)}
+                    }
+                }
+
             }
-
-            $ForestEvent = Get-EventLog -LogName "DNS Server" -ErrorAction 0 | Where-Object {($_.InstanceId -like 4500) -and ($_.Message -like '*Forest*')}
-            while (($ForestEvent -eq $null)){
-                Start-Sleep 10
-                $ForestEvent = Get-EventLog -LogName "DNS Server" -ErrorAction 0 | Where-Object {($_.InstanceId -like 4500) -and ($_.Message -like '*Forest*')}
-            }
-        }
-
-        ELSE {
-            $DNSLoadedEvent = Get-EventLog -LogName "DNS Server" -ErrorAction 0 | Where-Object {($_.InstanceId -like 4)}
-            while (($DNSLoadedEvent -eq $null)){
-                Start-Sleep 10
-                $DNSLoadedEvent = Get-EventLog -LogName "DNS Server" -ErrorAction 0 | Where-Object {($_.InstanceId -like 4)}
-            }
+            GetScript =  { @{} }
+            TestScript = { $false}
         }
        
         DnsServerADZone ReverseADZone
@@ -66,6 +76,7 @@
             DynamicUpdate = 'Secure'
             Ensure           = 'Present'
             ReplicationScope = 'Domain'
+            DependsOn = '[Script]CheckDNS'
         }
 
         DnsRecordPtr DCPtrRecord
